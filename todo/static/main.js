772ddd8566f657ff1618cec2bacf6711156fef90
Andres Arevalo <andresfelipear@gmail.com>
2021-10-28 21:52:49 -0700

    first commit

---
+++ todo/static/main.js
@@ -0,0 +1,6 @@
+completed = document.getElementById('completeMain');
+completed2 = document.getElementById('complete')
+deleted = d
+completed.addEventListener('click', ()=>{
+    completed2.click();
+})
